785a57e5eb42a9e06600a9050d45f074ba9dd3a1
Gustavo Costa <gustavo.ghcosta2@gmail.com>
2025-09-07 17:45:01 -0300

    Task_003 - Feito TDD do registro e login. Implementado JWT.

--- NutriTrack.Domains/Enums/EnumGenero.cs
+++ NutriTrack.Domains/Enums/EnumGenero.cs
@@ -4,5 +4,6 @@
     {
         Masculino = 1,
         Feminino  = 2,
+        Outros = 3,
     }
 }
---
+++ NutriTrack.Tests/TDD_Registro_Users/UserRegisterTdd.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Moq;
+using Nest;
+using NutriTrack_Domains.Dtos;
+using NutriTrack_Domains.Enums;
+using NutriTrack_Domains.Interfaces.Repository;
+using NutriTrack_Domains.Tables.UsersTb;
+using NutriTrack_Services.UserServices;
+
+namespace NutriTrack_Tests.TDD
+{
+    public class RegistroServiceTests
+    {
+        [Fact]
+        public async Task RegistrarAsync_QuandoEmailJaExiste_DeveRetornarFalha()
+        {
+            var dataNascimento = new DateOnly(1995, 10, 20);
+            var mockRepo = new Mock<NutriTrack_Domains.Interfaces.Repository.IRepository<Users>>();
+            var mockJWT = new Mock<IConfiguration>();
+
+            var dto = new RegisterUserDto
+            {
+                Nome = "Gustavo",
+                Sobrenome = "Costa",
+                Email = "gusta@email.com",
+                DataNascimento = dataNascimento,
+                Genero = EnumGenero.Masculino,
+                Senha = "senha123",
+                AlturaEmCm = 180,
+                PesoEmKg = 72,
+                NivelAtividade = EnumNivelDeAtividade.AtividadeModerada,
+                Objetivo = EnumObjetivo.GanharMassa,
+            };
+
+            mockRepo.Setup(r => r.AddAsync(It.IsAny<Users>())).Returns(Task.CompletedTask);
+
+            var service = new RegisterAndLoginServ(mockRepo.Object, mockJWT.Object);
+
+            var exception = await Record.ExceptionAsync(() => service.RegisterUser(dto));
+
+            Assert.Null(exception);
+        }
+    }
+}
