971bf2a92539ee5270a87629c94d6014a91915f8
Gustavo Costa <gustavo.ghcosta2@gmail.com>
2025-10-31 19:44:53 -0300

    TASK005 - Configurando cálculo da taxa basal

---
+++ NutriTrack.Domains/Dtos/UserProfileDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NutriTrack_Domains.Dtos
+{
+    public class UserProfileDto
+    {
+        public string NomeCompleto { get; set; }
+        public string Objetivo { get; set; } 
+        public int MetaCalorias { get; set; }
+        public int MetaProteinas { get; set; }
+        public int MetaCarboidratos { get; set; }
+        public int MetaGorduras { get; set; }
+
+    }
+}
