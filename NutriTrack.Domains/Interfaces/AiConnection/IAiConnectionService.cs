0d0ace685650ac28dbdccead12186cac9073d89f
Gustavo Costa <gustavo.ghcosta2@gmail.com>
2025-10-31 20:43:00 -0300

    Task_006 - Backend integração GEMINI.

---
+++ NutriTrack.Domains/Interfaces/AiConnection/IAiConnectionService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NutriTrack_Domains.Interfaces.AiConnection
+{
+    public interface IAiConnectionService
+    {
+
+        Task<string> GeminiConnection(string pergunta);
+
+    }
+}
